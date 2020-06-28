c95c5cda055c1744dcda4f7b0f46ebe281f97d8e
apsteinmetz <apsteinmetz@yahoo.com>
2020-06-28 13:47:51 -0400

    update

--- bk_to_spotify.R
+++ bk_to_spotify.R
@@ -3,7 +3,7 @@
 library(tidyverse)
 library(spotifyr)
 # relies on spotify credentials stored in system environment variables
-play_date <- "2020-06-20"
+play_date <- "2020-06-27"
 song_file <- paste0("raw_bk_playlists/bk_",play_date,".txt")
 show_name <-paste0("Blackhole_",play_date)
 
