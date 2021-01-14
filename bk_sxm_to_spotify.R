c1edbc6da1f7bd42baa75898a6da17fd2b715ad1
apsteinmetz <apsteinmetz@yahoo.com>
2021-01-14 15:39:52 -0500

    update

--- bk_sxm_to_spotify.R
+++ bk_sxm_to_spotify.R
@@ -1,11 +1,11 @@
 # make playlist for spotify from Bill Kelly's
 # SiriusXM playlist posts on Facebook
-# devtools::install_github('charlie86/spotifyr')
+#devtools::install_github('charlie86/spotifyr')
 library(tidyverse)
 library(spotifyr)
 library(spotfuzz)
 # relies on spotify credentials stored in system environment variables
-play_date <- "2020-12-19"
+play_date <- "2020_top_40"
 song_file <- paste0("raw_bk_playlists/bk_",play_date,".txt")
 show_name <-paste0("Blackhole_",play_date)
 
@@ -32,20 +32,7 @@ raw_playlist <- read_delim(song_file,delim='\"',
    mutate_all(str_trim) %>%
    {.}
 
-# MAIN FLOW OF PROGRAM --------------------------------------------------
-# SEARCH FOR SONG AT SPOTIFY AND RETRIEVE URI FOR PLAYLIST
-#why doesn't this work?
-#new_playlist <- playlist %>% 
-#   mutate(track_uri = get_track_uri(paste0("track:",song," artist:",artist)))
-# so do it the old-fashioned way
-print("Getting Track URIs")
-
-
-playlist <- raw_playlist
-uri_list <- fuzzy_search_spotify(playlist$artist,
-                                 playlist$song,
-                                 progress = TRUE)
-playlist <- bind_cols(playlist,uri_list) %>% select(song,spot_track,everything())
+raw_playlist <- raw_playlist %>% mutate(artist = str_trim(str_remove(artist,"[0-9]{0,2}\\)")))
 
 # manually remove false positives by row number
 # this is needed because my validation for correct song is lousy
@@ -64,6 +51,23 @@ fix_false_negative <- function(rownum,source = raw_playlist,target = playlist){
    target$song[rownum] <- source$song[rownum]
    return(target)
 }
+
+# MAIN FLOW OF PROGRAM --------------------------------------------------
+# SEARCH FOR SONG AT SPOTIFY AND RETRIEVE URI FOR PLAYLIST
+#why doesn't this work?
+#new_playlist <- playlist %>% 
+#   mutate(track_uri = get_track_uri(paste0("track:",song," artist:",artist)))
+# so do it the old-fashioned way
+print("Getting Track URIs")
+
+
+playlist <- raw_playlist
+uri_list <- fuzzy_search_spotify(playlist$artist,
+                                 playlist$song,
+                                 progress = TRUE)
+playlist <- bind_cols(playlist,uri_list) %>% select(song,spot_track,everything())
+
+
 
 available_tracks <- playlist %>% filter(!is.na(track_uri)) %>% pull(track_uri)
 missing_tracks <- playlist %>% filter(is.na(track_uri)) %>% 
