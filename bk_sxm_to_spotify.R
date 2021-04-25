33c92fb9c144a21909e4a2b60bd927a6bc30ed91
apsteinmetz <apsteinmetz@yahoo.com>
2021-04-25 15:15:38 -0400

    update

--- bk_sxm_to_spotify.R
+++ bk_sxm_to_spotify.R
@@ -6,7 +6,7 @@ library(spotifyr)
 library(spotfuzz)
 
 # relies on spotify credentials stored in system environment variables
-play_date <- "2021-04-03"
+play_date <- "2021-04-24"
 song_file <- paste0("raw_bk_playlists/bk_",play_date,".txt")
 show_name <-paste0("Blackhole_",play_date)
 
