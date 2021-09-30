3f3d53e6ab5a107fdfbac1cb7ceaa06e453fd66e
apsteinmetz <apsteinmetz@yahoo.com>
2021-09-30 11:04:19 -0400

    update

--- set secret token.R
+++ set secret token.R
@@ -1,6 +1,5 @@
 library(spotifyr)
 
-
 ac <- get_spotify_access_token(client_id = '8db2b0a52b7045758fa428f8b2f8fdb8',
                                          client_secret = 'e283b6aafe8e42a6aeb94112314f9615')
 
